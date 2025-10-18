56b2a95dea9bc798bf2d8d497adaf30004091a97
shoqqan <tataev.shokan@gmail.com>
2025-10-18 23:06:55 +0500

    [chore]: initialize project

---
+++ src/main/kotlin/dev/shoqan/fitness_app/FitnessAppApplication.kt
@@ -0,0 +1,11 @@
+package dev.shoqan.fitness_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitnessAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<FitnessAppApplication>(*args)
+}
